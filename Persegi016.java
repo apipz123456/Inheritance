a259407964d130239da774f28bd224329ab5da09
Fauzan Afif Lutfiansah <113918151+apipz123456@users.noreply.github.com>
2023-08-09 15:29:05 +0700

    Add files via upload

---
+++ Persegi016.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inheritance;
+
+/**
+ *
+ * @author Apipz
+ */
+class Persegi016 extends Bangundatar016{
+
+    int sisi;
+    @Override
+    float luas(){
+        float luas = sisi * sisi;
+        System.out.println("luasnya persegi panjang adalah "+ luas);
+        return 0;
+}
+    @Override
+    float keliling(){
+        float luas = 4 * sisi;
+        System.out.println("keliling persegi panjang adalah "+ luas);
+        return 0;}
+}
